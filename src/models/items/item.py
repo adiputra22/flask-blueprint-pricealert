34994ed0cf2adf3513f28248146e709d603bb850
adiputra22 <tuts.adiputra@gmail.com>
2018-03-26 14:42:11 +0700

    setup to scrap data price with beautifulsoup

--- src/models/items/item.py
+++ src/models/items/item.py
@@ -1,8 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
 class Item(object):
-    def __init__(self, name, price, url):
+    def __init__(self, name, url, store):
         self.name = name
-        self.price = price
         self.url = url
+        self.store = store
+
+        tag_name = store.tag_name
+        query = store.query
+
+        self.price = self.load_price(tag_name, query)
 
     def __repr__(self):
         return "<Item {} with url {} and price {}>".format(self.name, self.url, self.price)
+
+    def load_price(self, tag_name, query):
+        request = requests.get(self.url)
+        content = request.content
+        soup = BeautifulSoup(content, "html.parser")
+        element = soup.find(tag_name, query)
+        string_price = element.text.strip()
+
+        pattern = re.compile("(\d+.\d+)")
+        match = pattern.search(string_price)
+
+        return match.group()
