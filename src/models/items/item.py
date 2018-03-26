32e05ddeb301134b1c5315297a0ec3aaa381bba6
adiputra22 <tuts.adiputra@gmail.com>
2018-03-26 15:38:39 +0700

    adding _id field to items, stores and alerts model

--- src/models/items/item.py
+++ src/models/items/item.py
@@ -1,3 +1,4 @@
+import uuid
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -6,7 +7,7 @@ from src.common.database import Database
 
 
 class Item(object):
-    def __init__(self, name, url, store):
+    def __init__(self, name, url, store, _id=None):
         self.name = name
         self.url = url
         self.store = store
@@ -15,6 +16,7 @@ class Item(object):
         query = store.query
 
         self.price = self.load_price(tag_name, query)
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
         return "<Item {} with url {} and price {}>".format(self.name, self.url, self.price)
@@ -37,5 +39,6 @@ class Item(object):
     def json(self):
         return {
             "name": self.name,
-            "url": self.url
+            "url": self.url,
+            "_id": self._id
         }
