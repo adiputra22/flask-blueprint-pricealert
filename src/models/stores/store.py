6b861832725a46bc47a635670fc2acc427e72b2b
adiputra22 <tuts.adiputra@gmail.com>
2018-03-26 15:34:07 +0700

    update the field of store

--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -1,7 +1,9 @@
 class Store(object):
-    def __init__(self, name, url_prefix):
+    def __init__(self, name, url_prefix, tag_name, query):
         self.name = name
         self.url_prefix = url_prefix
+        self.tag_name = tag_name
+        self.query = query
 
     def __repr__(self):
         return "<Store {} with url prefix {}>".format(self.name, self.url_prefix)
